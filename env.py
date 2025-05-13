3738e09426ae6918a1c3d9e8e1ea71cdb882cc9d
Page0526 <phuongtrang2652004@gmail.com>
2025-05-13 10:26:18 +0700

    UPDATE: - file organization - add agents

--- env.py
+++ env.py
@@ -366,7 +366,7 @@ if __name__=="__main__":
 
     # Agents
     # Initialize agents
-    from greedyagent import GreedyAgents as Agents
+    from agents.greedyagent import GreedyAgents as Agents
     agents = Agents()   # You should define a default parameters here
     agents.init_agents(state) # You have a change to init states which can be used or not. Depend on your choice
     print("Agents initialized.")
